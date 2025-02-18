d39df5cdd1dba4c868a7101b8c259f35fbb137e6
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2025-02-18 12:08:02 +0100

    Tworzenie metod.

---
+++ Comarch20250217/Calculator/Calc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comarch20250217.Calculator
+{
+    internal class Calc
+    {
+        public int Add(int x, int y)
+        {
+            int wynik = x + y;
+            return wynik;
+        }
+
+        public int Substract(int x, int y)
+        {
+            int wynik = x - y;
+            return wynik;
+        }
+        
+    }
+}
--- Comarch20250217/Program.cs
+++ Comarch20250217/Program.cs
@@ -3,8 +3,11 @@
  * File: Program.cs
  */
 
+using Comarch20250217.Calculator;
 using System;
 using System.Net.Sockets;
+
+namespace Comarch20250217;
 
 /// <summary>
 /// Jest to klasa startowa aplikacji. 
@@ -18,51 +21,82 @@ internal class Program
     /// <seealso cref=""/>
     private static void Main(string[] args)
     {
+        ConsoleKeyInfo znakKontynuowac;
+        do
+        {
+            DisplayMenu();
+
+            string? sWybor = Console.ReadLine();
+            bool czyMoznaOdczytac = int.TryParse(sWybor, out int wybor);
+
+            if (czyMoznaOdczytac)
+            {
+                int x, y;
+                Calc kalkulator = new Calc();
+
+                switch (wybor)
+                {
+                    case 1:
+                        OdczytajXY(out x, out y);
+                        int suma = kalkulator.Add(x, y);
+                        Console.WriteLine($"Wynik dodawania {x} oraz {y} to {suma}");
+                        break;
+                    case 2:
+                        OdczytajXY(out x, out y);
+                        Console.WriteLine($"Wynik odejmowawania {x} oraz {y} to {kalkulator.Substract(x, y)}");
+                        break;
+                    case 3:
+                        OdczytajXY(out x, out y);
+                        Console.WriteLine($"Wynik mnożenia {x} oraz {y} to {x * y}");
+                        break;
+                    case 4:
+                        OdczytajXY(out x, out y);
+                        Console.WriteLine($"Wynik dzielenia {x} oraz {y} to {x / (float)y}");
+                        break;
+                    default:
+                        ShowError("Wybór jest nieprawidłowy!");
+                        break;
+                }
+            }
+            else
+            {
+                ShowError("Podana wartość jest nieprawidłowa!");
+            }
+            Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("Czy chcesz wykonać kolejną operację? [T|n]");
+            znakKontynuowac = Console.ReadKey();
+
+        } while (znakKontynuowac.Key == ConsoleKey.T);
+
+    }
+
+    private static void OdczytajXY(out int x, out int y)
+    {
+        Console.Write("Podaj X: ");
+        x = int.Parse(Console.ReadLine()!);
+        Console.Write("Podaj Y: ");
+        y = int.Parse(Console.ReadLine()!);
+    }
+
+    private static void DisplayMenu()
+    {
+        Console.Clear();
         Console.WriteLine("KALKULATOR 1.0");
         Console.WriteLine("1. Dodawanie");
         Console.WriteLine("2. Odejmowanie");
         Console.WriteLine("3. Mnożenie");
         Console.WriteLine("4. Dzielenie");
         Console.Write("Wybierz pozycję: ");
-
-        string? sWybor = Console.ReadLine();
-        bool czyMoznaOdczytac = int.TryParse(sWybor, out int wybor);
-
-        if (czyMoznaOdczytac)
-        {
-            Console.Write("Podaj X: ");
-            int x = int.Parse(Console.ReadLine()!);
-
-            Console.Write("Podaj Y: ");
-            int y = int.Parse(Console.ReadLine()!);
-
-            switch (wybor)
-            {
-                case 1:
-                    Console.WriteLine($"Wynik dodawania {x} oraz {y} to {x + y}");
-                    break;
-                case 2:
-                    Console.WriteLine($"Wynik odejmowawania {x} oraz {y} to {x - y}");
-                    break;
-                case 3:
-                    Console.WriteLine($"Wynik mnożenia {x} oraz {y} to {x * y}");
-                    break;
-                case 4:
-                    Console.WriteLine($"Wynik dzielenia {x} oraz {y} to {x / (float)y}");
-                    break;
-                default:
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Wybór jest nieprawidłowy!");
-                    Console.ResetColor();
-                    break;
-            }
-        }
-        else
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Podana wartość jest nieprawidłowa!");
-            Console.ResetColor();
-        }
-
     }
+
+    private static void ShowError(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+
 }
+
